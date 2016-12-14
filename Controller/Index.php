df040510e6859c880f4dabc4abf10646c2b21af5
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-12-14 13:38:42 +0100

    First commit

---
+++ Controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kodbruket\GoToSku\Controller;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('x');
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Kodbruket_GoToSku',
+    __DIR__
+);
